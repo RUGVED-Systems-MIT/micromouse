d973fcddc365eae5705d6e14810500ed9f91b48f
Piyush-M01 <modipiyush2001@gmail.com>
2021-12-04 02:55:28 +0530

    updated code

--- src/goalNode.cpp
+++ src/goalNode.cpp
@@ -1,28 +1,119 @@
  #include "ros/ros.h"
  #include "micromouse/position.h"
+ #include "sensor_msgs/LaserScan.h"
  #include<cstdlib>
+ #include<vector>
+
+class client
+{
+    public:
+        void laser_callback(const sensor_msgs::LaserScan::ConstPtr& scan_msg);
+        client(ros::NodeHandle& nh);
+        void check_wall(float& maxl,float& maxf, float& maxr);
+        float goal_x,goal_y;
+        float current_x=-1.35,current_y=1.227;
+        
+        // std::vector<std::vector<int,int>>{{14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14},
+        // {13,12,11,10,9,8,7,6,6,7,8,9,10,11,12,13},
+        // {12,11,10,9,8,7,6,5,5,6,7,8,9,10,11,12},
+        // {11,10,9,8,7,6,5,4,4,5,6,7,8,9,10,11},
+        // {10,9,8,7,6,5,4,3,3,4,5,6,7,8,9,10},
+        // {9,8,7,6,5,4,3,2,2,3,4,5,6,7,8,9},
+        // {8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8},
+        // {7,6,5,4,3,2,1,0,0,1,2,3,4,5,6,7},
+        // {7,6,5,4,3,2,1,0,0,1,2,3,4,5,6,7},
+        // {8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8},
+        // {9,8,7,6,5,4,3,2,2,3,4,5,6,7,8,9},
+        // {10,9,8,7,6,5,4,3,3,4,5,6,7,8,9,10},
+        // {11,10,9,8,7,6,5,4,4,5,6,7,8,9,10,11},
+        // {12,11,10,9,8,7,6,5,5,6,7,8,9,10,11,12},
+        // {13,12,11,10,9,8,7,6,6,7,8,9,10,11,12,13},
+        // {14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14}};
+    private:
+        ros::NodeHandle nh;
+        sensor_msgs::LaserScan laser_msg;
+        ros::Subscriber laser_subscriber;
+
+};
+
+client::client(ros::NodeHandle& nh)
+{
+    this->laser_subscriber = nh.subscribe<sensor_msgs::LaserScan>("/my_mm_robot/laser/scan", 100, &client::laser_callback,this);
+}
+
+void client::check_wall(float& maxl,float& maxf, float& maxr)
+{
+    if(maxl>0.2)
+    {
+        this->goal_x=this->current_x+0.16;
+    }
+    else if(maxf>0.2)
+    {
+        this->goal_y=this->current_y-0.16;
+    }
+    else if(maxr>0.2)
+    {
+        this->goal_x=this->current_x-0.16;
+    }
+}
+
+void client::laser_callback(const sensor_msgs::LaserScan::ConstPtr& scan_msg)
+{ 
+    std::vector<float> left;
+    std::vector<float> right;
+    std::vector<float> front;
+    float maxf=0,maxl=0,maxr=0;
+    for (int i = 0; i < scan_msg->ranges.size(); i++) 
+    {
+        if(i<120)
+        {
+            right.push_back(scan_msg->ranges[i]);
+            if (maxr<scan_msg->ranges[i])
+                maxr=scan_msg->ranges[i];
+        }
+        else if(i>=240  &&  i<=480)
+        {
+            front.push_back(scan_msg->ranges[i]);
+            if (maxf<scan_msg->ranges[i])
+                maxf=scan_msg->ranges[i];
+        }
+        else if(i>=600)
+        {
+            left.push_back(scan_msg->ranges[i]);
+            if (maxl<scan_msg->ranges[i])
+            maxl=scan_msg->ranges[i];
+        }
+    }
+    ROS_INFO("maxf %f",maxf);
+    ROS_INFO("maxr %f",maxr);
+    ROS_INFO("maxl %f",maxl);
+    check_wall(maxl,maxf,maxr);
+}
 
 int main(int argc,char **argv)
 {
     ros::init(argc,argv,"goalnode_client");
-    if(argc!=4)
+    // if(argc!=4)
+    // {
+    //     ROS_INFO("usage: get the position x y");
+    //     return 1;
+    // }
+    ros::NodeHandle n;
+    ros::ServiceClient cli=n.serviceClient<micromouse::position>("position");
+    micromouse::position srv;
+    client ob(n);
+    srv.request.x=ob.goal_x;
+    srv.request.y=ob.goal_y;
+    if (cli.call(srv))
     {
-        ROS_INFO("usage: get the position x y");
-        return 1;
+            ROS_INFO("Position status: [%f]",srv.response.goal_received[0]);
+            ob.current_x=ob.goal_x;
     }
-ros::NodeHandle n;
-ros::ServiceClient client=n.serviceClient<micromouse::position>("position");
-micromouse::position srv;
-srv.request.x=atoll(argv[1]);
-srv.request.y=atoll(argv[2]);
-if (client.call(srv))
-{
-        ROS_INFO("Position status: [%d]",srv.response.goal_received);
-}
-else
-{
-        ROS_ERROR("Failed to call service");
-        return 1;
-}
+    else
+    {
+            ROS_ERROR("Failed to call service");
+            return 1;
+    }
+    ros::spin();
 return 0;
 }
--- src/goalPos.cpp
+++ src/goalPos.cpp
@@ -13,42 +13,27 @@ class controller
 {
 
   public:
-
     controller(ros::NodeHandle &nh);
-
     void odomCB(const nav_msgs::Odometry::ConstPtr& msg);
     bool get_position(micromouse::position::Request  &req, micromouse::position::Response &res);
-
-  protected:
-
     double kp,kd, x, y, theta;
-    //double last_time,p,i,d,st;
     double prev_error,prev_theta_err;
+    float yaw1;
 
   private:
 
     ros::Publisher pub;
     ros::Subscriber sub;
     ros::ServiceServer server;
+    ros::NodeHandle nh;
 
 };
 
 controller::controller(ros::NodeHandle &nh)
 {
   this->server = nh.advertiseService("position", &controller::get_position,this);
-  this->pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
-  this->sub = nh.subscribe<nav_msgs::Odometry>("/odom", 100, &controller::odomCB, this);
-
-
-  this->x=0;
-  this->y=0;
-
   this->kp=1.0;
   this->kd=0.0;
-  //this.ki=0.0;
-  this->prev_error=0;
-  this->prev_theta_err=0;
-  //this->st=0.01;
 }
 
 void controller::odomCB(const nav_msgs::Odometry::ConstPtr& msg)
@@ -61,38 +46,10 @@ void controller::odomCB(const nav_msgs::Odometry::ConstPtr& msg)
   
   double distance_err= sqrt(pow(this->x-msg->pose.pose.position.x,2)+pow(this->y-msg->pose.pose.position.y,2));
   this->theta=M_PI/2+atan2(this->y-msg->pose.pose.position.y,this->x-msg->pose.pose.position.x);
-
+  this->yaw1=yaw;
   double theta_err=this->theta-yaw;
   printf("theta_err: %f",theta_err);
   std::cout<<std::endl;
-
-  // if(abs(theta_err)>0.05)
-  // {
-  //   //speed.angular.z=5*theta_err+(0.2*abs(prev_theta_err-theta_err))/0.05;
-  //   if(abs(theta_err)>3.14)
-  //   speed.angular.z=-10*theta_err;
-  //   else
-  //   speed.angular.z=10*theta_err;
-  //   //speed.linear.x=0.02;
-  //   //prev_theta_err=theta_err;
-  //   //speed.linear.x=0.05;
-  // }
-  // else
-  // {
-  //   speed.angular.z = 0;
-  //   if(distance_err>0.1)
-  //   { 
-  //     //speed.angular.y=0;
-  //     //speed.linear.x=(2*abs(prev_error-distance_err))/0.05;
-  //     //prev_error=distance_err;
-  //     speed.linear.x=5*theta_err;
-  //   }
-  //   else
-  //   {
-  //     speed.linear.x=0;
-  //   }
-  // }
-
     if(distance_err>0.1)
     { 
       speed.angular.y=0;
@@ -117,10 +74,12 @@ bool controller::get_position(micromouse::position::Request  &req,
   {
     this->x=req.x;
     this->y=req.y;
-    res.goal_received=true;
+    res.goal_received={1,this->yaw1};
+    this->pub = this->nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
+    this->sub = this->nh.subscribe<nav_msgs::Odometry>("/odom", 100, &controller::odomCB, this);
   }
   else
-    res.goal_received=false;
+    res.goal_received={0,this->yaw1};
   return true;
 }
 
